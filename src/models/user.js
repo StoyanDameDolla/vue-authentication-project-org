d33b68293b7c055fc2bae4b5e854c3487a2a09cb
StoyanDameDolla <stoyan.ch.stoyanov11@gmail.com>
2022-02-28 19:57:28 +0200

    Init commit on dev branch

---
+++ src/models/user.js
@@ -0,0 +1,8 @@
+export default class User {
+  // Constructor for generic user model definition - contains username, email, password
+  constructor(username, email, password) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+  }
+}
---
+++ src/views/Profile.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <header class="jumbotron">
+      <h3>
+        <strong>{{ currentUser.username }}</strong> User Profile
+      </h3>
+    </header>
+    <p>
+      <strong>Token:</strong>
+      {{ currentUser.accessToken.substring(0, 20) }} ...
+      {{ currentUser.accessToken.substr(currentUser.accessToken.length - 20) }}
+    </p>
+    <p>
+      <strong>Id:</strong>
+      {{ currentUser.id }}
+    </p>
+    <p>
+      <strong>Email:</strong>
+      {{ currentUser.email }}
+    </p>
+    <strong>Authorities:</strong>
+    <ul>
+      <li v-for="(role, index) in currentUser.roles" :key="index">
+        {{ role }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  /* If user is not logged in --> redirect user to Login page */
+  name: "User Profile",
+  computed: {
+    currentUser() {
+      return this.$store.state.auth.user;
+    },
+  },
+  mounted() {
+    if (!this.currentUser) {
+      this.$router.push("/login");
+    }
+  },
+};
+</script>
+
+<style scoped></style>
